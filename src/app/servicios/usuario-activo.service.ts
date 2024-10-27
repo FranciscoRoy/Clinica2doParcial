b0139e6fdf923462404e46cd586ad38ac538a767
Roy <franciscoroy.dm@gmail.com>
2024-10-27 19:51:24 -0300

    esperaingreso_act

--- src/app/servicios/usuario-activo.service.ts
+++ src/app/servicios/usuario-activo.service.ts
@@ -1,7 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 import { Gerente, Paciente, Profesional, Usuario, UsuariosinIngresar } from '../clases/usuario';
-import { VentanaActivaService } from './ventanaactiva.service';
 
 @Injectable({
   providedIn: 'root'
--- src/app/ventanasdinamicas/ingreso/ingreso.component.ts
+++ src/app/ventanasdinamicas/ingreso/ingreso.component.ts
@@ -46,41 +46,55 @@ ingreso(){
   }, 2000);
 }
 
+esperarDatosUsuario(){
+  const subscription = this.usuarioActivoService.usuarioActual$.subscribe((usuarioActivo) => {
+    if (usuarioActivo && usuarioActivo.getTipoUsuario() !== 'Invitado') {
+      this.ventanaActivaService.cambiarVentana('inicio');
+      subscription.unsubscribe();
+    } else {
+      this.ventanaActivaService.cambiarVentana('espera');
+    }
+  });
+}
+
 ingresarComoPaciente(){
   this.apiService.buscarPaciente(this.email,this.password).subscribe(
     (data: Paciente) => {
       this.usuarioBuscado = new Paciente (data.nombre, data.apellido, data.dni, data.email, '*******', data.foto);
-      this.usuarioBuscado.setValPend(data.valPend);
+      this.usuarioBuscado.setValPend(data.valPend); //actualiza las valoraciones que el usuario debe completar
+      // funcion para actualizar los chats recibidos this.usuarioBuscado.setChatsPend(data.chatsPend);
+      this.esperarDatosUsuario();
     },
     (error) => {
       console.error('Error:', error);
     }
   );
-  this.ventanaActivaService.navegar('inicio',6);
 }
 
 ingresarComoProfesional(){
+  this.ventanaActivaService.cambiarVentana('espera');
   this.apiService.buscarProfesional(this.email,this.password).subscribe(
     (data: Profesional) => {
       this.usuarioBuscado = new Profesional (data.nombre, data.apellido, data.dni, data.email, '*******', data.foto, '', '', '', '', '');
+      this.esperarDatosUsuario();
     },
     (error) => {
       console.error('Error:', error);
     }
   );
-  this.ventanaActivaService.navegar('inicio',6);
 }
 
 ingresarComoGerente(){
+  this.ventanaActivaService.cambiarVentana('espera');
   this.apiService.buscarGerente(this.email,this.password).subscribe(
     (data: Gerente) => {
       this.usuarioBuscado = new Gerente(data.nombre, data.apellido, data.dni, data.email, '*******', data.foto);
+      this.esperarDatosUsuario();
     },
     (error) => {
       console.error('Error:', error);
     }
   );
-  this.ventanaActivaService.navegar('inicio',6);
 }
 
 }
