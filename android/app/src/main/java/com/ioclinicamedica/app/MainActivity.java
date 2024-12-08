fd7975206cf864e12999cfa85bc84084977c4f84
Roy <franciscoroy.dm@gmail.com>
2024-12-08 13:05:21 -0300

    Ionic_compila

---
+++ android/app/src/main/java/com/ioclinicamedica/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.ioclinicamedica.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.ioclinicamedica.app',
+  appName: 'ioclinicamedica',
+  webDir: 'dist/clinicamedica/browser'
+};
+
+export default config;
